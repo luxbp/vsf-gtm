4e95c2c13d7248315a78ba4fe88368a70b3c3acd
Brian McDonald <bmcdonald@luxbp.com>
2021-07-08 12:13:40 -0400

    feat(product-data): use final price on item

--- helper/createProductData.ts
+++ helper/createProductData.ts
@@ -11,7 +11,7 @@ export default (product, opts: Record<string, any> = {}) => {
     name: product.name, // 'awesome t-shirt'
     quantity: product.qty || opts.qty, // 1 // the quantity REMOVED from the cart (not the quantity remaining in the cart)
     brand: product.brand || view.storeCode, // my-store // if there are multiple brands use the brand variable here
-    price: product.price, // 10.0 // make sure the price is a number ie 10.00 not $10.00
+    price: product.final_price, // 10.0 // make sure the price is a number ie 10.00 not $10.00
     variant: product.sku.split('-')[1] || null,
     category: createProductCategoryName(product), // 'clothes/shirts/t-shirts' // max five levels of hierarchy
     // requires product-scoped custom dimensions:
